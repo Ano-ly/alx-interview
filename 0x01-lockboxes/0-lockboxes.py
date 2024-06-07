d0b03e30579820f1ce9c31f41f3f27470f5e66a5
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-06-07 05:54:32 +0100

    Create Lockboxes files and directories

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Lockboxes are now opened"""
+
+
+def canUnlockAll(boxes):
+    """Check if all boxes can be unlocked"""
+
+    return (False)
---
+++ README.md
@@ -0,0 +1,1 @@
+An Interview from Alx
