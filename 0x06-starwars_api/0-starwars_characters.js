4d61a022cab97df27d439120fcd7908d41ba220a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-12 04:55:08 +0100

    Create dummy file

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      const newObj = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(newObj);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
