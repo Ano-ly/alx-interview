961e4ce97e339e83a6dea69b57b0f9849e3dc8f3
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-02 05:58:21 +0100

    task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Island Perimeter ccalculation"""
+
+
+def calc_sides(grid, r_idx, c_idx):
+    """Support function"""
+    sides = 0
+    if grid[r_idx][c_idx + 1] == 0:
+        sides += 1
+    if grid[r_idx][c_idx - 1] == 0:
+        sides += 1
+    if grid[r_idx - 1][c_idx] == 0:
+        sides += 1
+    if grid[r_idx + 1][c_idx] == 0:
+        sides += 1
+    return (sides)
+
+
+def island_perimeter(grid):
+    """Island perimeter function"""
+    if len(grid) == 0:
+        return (0)
+    len_rows = len(grid)
+    len_cols = len(grid[0])
+    i = 0
+    per = 0
+    while i < len_rows:
+        j = 0
+        while j < len_cols:
+            if grid[i][j] == 1:
+                sides = calc_sides(grid, i, j)
+                per += sides
+            j += 1
+        i += 1
+    return (per)
