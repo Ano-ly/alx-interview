2a9f0581569194b1e4ae8c6e8c5e0c0eec107905
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-02 13:07:47 +0100

    Edit

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,16 +2,28 @@
 """Island Perimeter ccalculation"""
 
 
-def calc_sides(grid, r_idx, c_idx):
+def calc_sides(grid, r_idx, c_idx, len_rows, len_cols):
     """Support function"""
     sides = 0
-    if grid[r_idx][c_idx + 1] == 0:
+    if c_idx != len_cols - 1:
+        if grid[r_idx][c_idx + 1] == 0:
+            sides += 1
+    else:
         sides += 1
-    if grid[r_idx][c_idx - 1] == 0:
+    if c_idx != 0:
+        if grid[r_idx][c_idx - 1] == 0:
+            sides += 1
+    else:
         sides += 1
-    if grid[r_idx - 1][c_idx] == 0:
+    if r_idx != 0:
+        if grid[r_idx - 1][c_idx] == 0:
+            sides += 1
+    else:
         sides += 1
-    if grid[r_idx + 1][c_idx] == 0:
+    if r_idx != len_rows - 1:
+        if grid[r_idx + 1][c_idx] == 0:
+            sides += 1
+    else:
         sides += 1
     return (sides)
 
@@ -28,7 +40,7 @@ def island_perimeter(grid):
         j = 0
         while j < len_cols:
             if grid[i][j] == 1:
-                sides = calc_sides(grid, i, j)
+                sides = calc_sides(grid, i, j, len_rows, len_cols)
                 per += sides
             j += 1
         i += 1
